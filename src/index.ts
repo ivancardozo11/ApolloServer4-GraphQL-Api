f05a7638d936ab1e52aa27a9d8784cca4c7147d5
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-10-11 23:58:01 +0200

    rest data source libraries added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { RESTDataSource, WillSendRequestOptions  } from '@apollo/datasource-rest';
+import type { KeyValueCache } from '@apollo/utils.keyvaluecache';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+
 
 const typeDefs = `
 
@@ -75,6 +78,34 @@ const books = [
       books: () => books,
     },
   };
+
+  class MoviesAPI extends RESTDataSource { // highlight-line
+    override baseURL = 'https://movies-api.example.com/';
+    private token: string;
+  
+    constructor(options: { token: string; cache: KeyValueCache }) {
+      super(options); // this sends our server's `cache` through
+      this.token = options.token;
+    }
+  
+    override willSendRequest(request: WillSendRequestOptions) {
+      request.headers['authorization'] = this.token;
+    }
+  
+    async getMovie(id: string): Promise<Movie> {
+      return this.get<Movie>(`movies/${encodeURIComponent(id)}`);
+    }
+  }
+  
+  // highlight-start
+  interface ContextValue {
+    token: string;
+    dataSources: {
+      moviesAPI: MoviesAPI;
+    };
+  }
+  // highlight-end
+  
 
 
   const server = new ApolloServer({
